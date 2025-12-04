34a4dd533d6f8b7491d507c014f3f48db08fcb41
fernandoortiz-maker <fernandopadronortiz@gmail.com>
2025-12-04 10:28:58 -0600

    cambios 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // <-- CORREGIDO: Ya somos vecinos, no hace falta decir "src"
+import App from './App.tsx'; // <-- CORREGIDO: Ya somos vecinos, no hace falta decir "src"
 import './App.css'; // <-- CORREGIDO: Busca el archivo que está al lado
 
 ReactDOM.createRoot(document.getElementById('root')).render(
