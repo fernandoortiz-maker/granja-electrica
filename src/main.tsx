e859c70fce79b3d71764ab686de7c6351031ed6a
JFPO08200 <23e06a09@unijv.edu.mx>
2025-12-04 10:44:37 -0600

    cambios 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx'; // <-- CORREGIDO: Ya somos vecinos, no hace falta decir "src"
 import './App.css'; // <-- CORREGIDO: Busca el archivo que está al lado
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
